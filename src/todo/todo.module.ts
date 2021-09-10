38c27b53a94119c7d4435b5aff3e287f0ade81eb
Paulina.Milewska <java.angular.sql@gmail.com>
2021-09-10 18:56:32 +0200

    Revert "add Auth0 security"
    This reverts commit 225aa154b11af044c834ba7f127ebfe2fdc9543f.

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,8 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoSchema } from './schemas/todo.schema';
-import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
   imports: [
@@ -12,12 +11,4 @@ import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
   providers: [TodoService],
   controllers: [TodoController]
 })
-export class TodoModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
-    consumer.apply(AuthenticationMiddleware).forRoutes(
-      { method: RequestMethod.POST, path: '/todo/task' },
-      { method: RequestMethod.PUT, path: '/todo/edit' },
-      { method: RequestMethod.DELETE, path: '/todo/delete' }
-    )
-  }
-}
+export class TodoModule {}
