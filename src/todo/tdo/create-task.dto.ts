17133deb1d5e5d7c9de098115aaa67cec1790241
Paulina.Milewska <java.angular.sql@gmail.com>
2021-09-12 21:08:14 +0200

    modify dto

--- src/todo/tdo/create-task.dto.ts
+++ src/todo/tdo/create-task.dto.ts
@@ -1,8 +1,9 @@
 export class CreateTaskDTO {
+    readonly taskId: string | null;
     readonly title: string;
     readonly description: string;
     readonly isDone: boolean;
-    readonly priority: number;
+    readonly priority: string;
     readonly startDate: string;
     readonly endDate: string;
 }
